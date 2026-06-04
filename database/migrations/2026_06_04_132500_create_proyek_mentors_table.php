21419d5082cfa2796f5045ec55d565057ee34855
Blqissafirra9 <soraa4518@gmail.com>
2026-06-04 19:35:47 +0700

    Add files via upload

---
+++ database/migrations/2026_06_04_132500_create_proyek_mentors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyek_mentors', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('mentor_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->string('judul_proyek');
+
+            $table->text('deskripsi')->nullable();
+
+            $table->string('file_proyek')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyek_mentors');
+    }
+};
